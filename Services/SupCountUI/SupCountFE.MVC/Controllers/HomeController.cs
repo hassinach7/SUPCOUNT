fa079aa1dfa7740612151f5b7079149e98ee3e20
hassina541 <hassinach95@gmail.com>
2025-05-12 18:09:53 +0200

    l'integration du template bootstrap

--- Services/SupCountUI/SupCountFE.MVC/Controllers/HomeController.cs
+++ Services/SupCountUI/SupCountFE.MVC/Controllers/HomeController.cs
@@ -1,32 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
-using SupCountFE.MVC.Models;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace SupCountFE.MVC.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
 }
--- Services/SupCountUI/SupCountFE.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SupCountFE.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
