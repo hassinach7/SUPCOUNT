ed421ca7f56feee2ecd65993d076d3ef3eee6000
hassina541 <hassinach95@gmail.com>
2025-05-12 17:46:46 +0200

    creation de projet front end en Asp .Net core MVC

---
+++ Services/SupCountUI/SupCountFE.MVC/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace SupCountFE.MVC.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
