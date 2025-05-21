c17612ec31b70dd3a95dcd9067b536b1ccb38539
hassina541 <hassinach95@gmail.com>
2025-05-21 22:28:43 +0200

    visbility roles

--- Services/SupCountUI/SupCountFE.MVC/Controllers/HomeController.cs
+++ Services/SupCountUI/SupCountFE.MVC/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
-﻿namespace SupCountFE.MVC.Controllers
+﻿using SupCountFE.MVC.Models;
+
+namespace SupCountFE.MVC.Controllers
 {
 	[Route("[controller]")]
     public class HomeController : Controller
 	{
-      
+        private readonly Helper helper;
+
+        public HomeController(Helper helper)
+        {
+            this.helper = helper;
+        }
+
         public IActionResult Index()
 		{
-			return View();
+            if(!helper.UserRoles.Contains("User"))
+            {
+                return RedirectToAction("List", "User");
+            }
+            return View();
 		}
 	}
 }
